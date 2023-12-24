f58da8706ded9cd8b08d5b1bc68e0877c926fb1b
Laqudee Des <24524492+laqudee@users.noreply.github.com>
2023-12-24 10:19:39 +0800

    format examples

--- examples/useFuturable.es.mjs
+++ examples/useFuturable.es.mjs
@@ -10,3 +10,21 @@ new Futurable((resolve) => {
 }).then((val) => {
   console.log('val: ', val)
 })
+
+Futurable.any([
+  new Futurable((resolve, reject) => setTimeout(() => reject(new Error('Whoops!')), 1000)),
+  new Futurable((resolve, reject) => setTimeout(() => resolve(1), 2000)),
+  new Futurable((resolve, reject) => setTimeout(() => resolve(3), 3000))
+]).then((val) => {
+  console.log('any-success-val: ', val)
+}) // 1
+
+Futurable.any([
+  new Futurable((resolve, reject) => setTimeout(() => reject(new Error('Ouch!')), 1000)),
+  new Futurable((resolve, reject) => setTimeout(() => reject(new Error('Error!')), 2000))
+]).catch((error) => {
+  // console.log('any-all reject-error: ', error);
+  console.log('name:', error.constructor.name) // AggregateError
+  console.log(error.errors[0]) // Error: Ouch!
+  console.log(error.errors[1]) // Error: Error!
+})
